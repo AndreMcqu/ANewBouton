d20ce27e4760084d8d2f29f955f16c923c76c6e1
andremcqu <mcquade.andre@gmail.com>
2022-05-13 14:40:49 +0200

    My frist personal component

---
+++ composant/comps.js
@@ -0,0 +1,27 @@
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+
+const Btn = (props) => {
+    return (
+        <TouchableOpacity style={styles.bouton}>
+            <Text style={styles.labeltxt}>{props.label}</Text>
+        </TouchableOpacity>
+    );
+}
+
+const styles = StyleSheet.create({  
+  bouton: {
+    backgroundColor: 'red',
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '60%',
+    height: 80,
+
+},
+labeltxt: {
+    color: 'black',
+    justifyContent: 'center'
+}
+});
+
+
+export default Btn;
